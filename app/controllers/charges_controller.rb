1036b9219ed2fa51e093f415a64c0e6abb778571
Tory Herman <tory.herman12@gmail.com>
2017-02-14 23:19:15 -0600

    add stripe

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,30 @@
+class ChargesController < ApplicationController
+  def new
+  end
+
+  def create
+    # Amount in cents
+    @amount = 1500
+
+    customer = Stripe::Customer.create(
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: 'Upgrade to Premium Membership',
+      currency: 'usd'
+    )
+
+    flash[:notice] = "Thanks for upgrading to a premium membership, #{current_user.first_name}!"
+    current_user.role = "premium"
+    current_user.save!
+    redirect_to edit_user_registration_path
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :wikis
+  resources :charges
 
   get 'welcome/index'
   root 'welcome#index'
