377e6afa192648b8680d8e70610678f52e084f55
Tory Herman <tory.herman12@gmail.com>
2017-02-27 22:25:14 -0600

    update devise views and mailers

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,11 @@ class User < ApplicationRecord
 
   enum role: [:standard, :premium, :admin]
 
+  def avatar_url(size)
+    gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
+
   private
 
   def init
